a1710f006abd9423644cadd6fab9e9ec12ee7df6
ashleywix <87287995+ashleywix@users.noreply.github.com>
2022-10-27 15:03:00 +1100

    Deployed to AWS and tested

--- lib/netskope_cloud_exchange-stack.ts
+++ lib/netskope_cloud_exchange-stack.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Construct} from 'constructs';
 
-export class NetskopeCloudExchangeStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
 
-    // The code that defines your stack goes here
+export class EcsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.Stack) {
+    super(scope, id);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NetskopeCloudExchangeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+// New VPC
+    const vpc = new cdk.aws_ec2.Vpc
+    (this, "MyVpc", {
+      maxAzs: 3
+
+    });
+// New Cluster
+    const cluster = new cdk.aws_ecs.Cluster(this, "MyCLuster", {
+      vpc: vpc
+
+    });
+// New ECS Fargate services sd required
+    new cdk.aws_ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster,
+      cpu: 512,
+      desiredCount: 4,
+      taskImageOptions: {image: cdk.aws_ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
+      memoryLimitMiB: 2048,
+      publicLoadBalancer: true
+       
+
+
+    });
+    // New EFS 
+    const fileSystem = new cdk.aws_efs.FileSystem(this, 'MyEfsFileSystem', {
+        vpc: new cdk.aws_ec2.Vpc(this, 'VPC'),
+        lifecyclePolicy: cdk.aws_efs.LifecyclePolicy.AFTER_14_DAYS, // files are not transitioned to infrequent access (IA) storage by default
+        performanceMode: cdk.aws_efs.PerformanceMode.GENERAL_PURPOSE, // default
+        outOfInfrequentAccessPolicy: cdk.aws_efs.OutOfInfrequentAccessPolicy.AFTER_1_ACCESS, // files are not transitioned back from (infrequent access) IA to primary storage by default
+      });
+  
   }
 }
